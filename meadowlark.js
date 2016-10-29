0a512087be7a217ceb6f25f991f2dc523e1bc66b
Panasenko <panasenkomaksim@gmail.com>
2016-10-29 18:17:14 +0300

    Добавлены модули работы с куками и сессиями

--- meadowlark.js
+++ meadowlark.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var formidable = require('formidable');
 var handlebars = require('express-handlebars').create({
     defaultLayout:'main',
     helpers: {
@@ -12,6 +13,7 @@ var handlebars = require('express-handlebars').create({
 });
 var fortune = require('./lib/fortune.js');
 var weather = require('./lib/weather.js');
+var credentials = require('./lib/credentials.js');
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
@@ -19,6 +21,19 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 app.use(require('body-parser').urlencoded({extended: true}));
+app.use(require('cookie-parser')(credentials.cookiesSecret));
+app.use(require('express-session')({
+    resave: false,
+    saveUninitialized: false,
+    secret: 'Секретный файл'
+}));
+app.use(function(req, res, next){
+    // if there's a flash message, transfer
+    // it to the context, then clear it
+    res.locals.flash = req.session.flash;
+    delete req.session.flash;
+    next();
+});
 app.use(function (req, res, next) {
     res.locals.showTests = app.get('evt') !== 'productoin' && req.query.test === '1';
     next();
@@ -30,13 +45,18 @@ app.use(function (req, res, next) {
 });
 
 app.get('/', function (req, res) {
+    req.session.name = 'Anonimus';
+    var colorShema = req.session.colorShema || 'dark';
+    console.log(req.session.name);
     res.render('home');
 });
 
 app.get('/about', function (req, res) {
+    res.cookie('monstroBit','vsykayaDich');
     res.render('about', {fortune: fortune.getFortune(),
         pageTestScript : '/qa/tests-about.js'
     });
+    console.log(req.cookies.monstroBit);
 });
 
 app.get('/tours/hood-river', function (req, res) {
@@ -69,12 +89,73 @@ app.get('/thank-you', function(req, res){
 app.get('/newsletter', function(req, res){
     res.render('newsletter', { csrf: 'CSRF token goes here' });
 });
+
 app.post('/process', function(req, res){
-    console.log('Form ' + req.query.form);
-    console.log('token ' + req.body._csrf);
-    console.log('name ' + req.body.name);
-    console.log('email ' + req.body.email);
-    res.redirect('/thank-you');
+    if(req.xhr || req.accepts('json,html')==='json'){
+        // if there were an error, we would send { error: 'error description' }
+        res.send({ success: true });
+    } else {
+        // if there were an error, we would redirect to an error page
+        res.redirect(303, '/thank-you');
+    }
+});
+
+
+app.get('/contest/vacation-photo', function(req, res){
+    var now = new Date();
+    res.render('contest/vacation-photo', { year: now.getFullYear(), month: now.getMonth() });
+});
+
+app.post('/contest/vacation-photo/:year/:month', function(req, res){
+    var form = new formidable.IncomingForm();
+    form.parse(req, function(err, fields, files){
+        if(err) return res.redirect(303, '/error');
+        console.log('received fields:');
+        console.log(fields);
+        console.log('received files:');
+        console.log(files);
+        res.redirect(303, '/thank-you');
+    });
+});
+
+function NewsletterSignup(){
+}
+NewsletterSignup.prototype.save = function(cb){
+    cb();
+};
+
+var VALID_EMAIL_REGEX = /^\/d/;
+
+app.post('/newsletter', function(req, res){
+    var name = req.body.name || '', email = req.body.email || '';
+    // input validation
+    if(!email.match(VALID_EMAIL_REGEX)) {
+        if(req.xhr) return res.json({ error: 'Invalid name email address.' });
+        req.session.flash = {
+            type: 'danger',
+            intro: 'Ошибка валидации!',
+            message: 'кривой адрес.',
+        };
+        return res.redirect(303, '/newsletter/archive');
+    }
+    new NewsletterSignup({ name: name, email: email }).save(function(err){
+        if(err) {
+            if(req.xhr) return res.json({ error: 'Database error.' });
+            req.session.flash = {
+                type: 'danger',
+                intro: 'Database error!',
+                message: 'There was a database error; please try again later.',
+            };
+            return res.redirect(303, '/newsletter/archive');
+        }
+        if(req.xhr) return res.json({ success: true });
+        req.session.flash = {
+            type: 'success',
+            intro: 'Thank you!',
+            message: 'You have now been signed up for the newsletter.',
+        };
+        return res.redirect(303, '/newsletter/archive');
+    });
 });
 
 
@@ -89,7 +170,8 @@ app.use(function (err, req, res, next) {
     res.render('500');
 });
 
+console.log(app.get(''))
 
 app.listen(app.get('port'), function () {
-    console.log('Express запущен на порту ' + app.get('port'));
+    console.log('Express запущен на порту ' + app.get('port') + " " + app.get('env'));
 });
