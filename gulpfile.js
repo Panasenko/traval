8088cf465a6f54776e4346e4832c3d8576087d8b
Panasenko <panasenkomaksim@gmail.com>
2016-11-13 18:57:24 +0200

    Добавлена поддержка нативного API экспресс

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
 var gulp = require('gulp'),
-    jshint = require('gulp-jshint');
+    jshint = require('gulp-jshint'),
+    mocha = require('gulp-mocha');
 
 gulp.task('lint', function() {
     return gulp.src('*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
+
+gulp.task('mocha', function () {
+       return gulp.src('\public\qa\tests-about.js', {read: false})
+            .pipe(mocha({reporter: 'nyan'}));
+    }
+
+);
--- meadowlark.js
+++ meadowlark.js
@@ -11,21 +11,143 @@ var handlebars = require('express-handlebars').create({
         }
     }
 });
+var connect = require('connect');
 var fortune = require('./lib/fortune.js');
 var weather = require('./lib/weather.js');
 var credentials = require('./lib/credentials.js');
+var mongoose = require('mongoose');
+var Vacation = require('./models/vacation.js');
+var VacationInSeasonListener = require('./models/vacationInSeasonListener.js');
+
+var MongoSessionStore = require('session-mongoose')(require('connect'));
+var sessionStore = new MongoSessionStore({ url: credentials.mongo[app.get('env')].connectionString });
+
+var opts = {
+    server: {
+        socketOptions: {keepAlive:1}
+    }
+};
+
+switch (app.get('env')){
+    case 'development':
+        mongoose.connect(credentials.mongo.development.connectionString, opts);
+        break;
+    case 'production':
+        mongoose.connect(credentials.mongo.production.connectionString, opts);
+        break;
+    default:
+        new Error('Неизвестная среда выполнения: ' + app.get('env'));
+}
+
+Vacation.find(function(err, vacations){
+    if(vacations.length) return;
+
+    new Vacation({
+        name: 'Hood River Day Trip',
+        slug: 'hood-river-day-trip',
+        category: 'Day Trip',
+        sku: 'HR199',
+        description: 'Spend a day sailing on the Columbia and ' +
+        'enjoying craft beers in Hood River!',
+        priceInCents: 9995,
+        tags: ['day trip', 'hood river', 'sailing', 'windsurfing', 'breweries'],
+        inSeason: true,
+        maximumGuests: 16,
+        available: true,
+        packagesSold: 0,
+    }).save();
+
+    new Vacation({
+        name: 'Oregon Coast Getaway',
+        slug: 'oregon-coast-getaway',
+        category: 'Weekend Getaway',
+        sku: 'OC39',
+        description: 'Enjoy the ocean air and quaint coastal towns!',
+        priceInCents: 269995,
+        tags: ['weekend getaway', 'oregon coast', 'beachcombing'],
+        inSeason: false,
+        maximumGuests: 8,
+        available: false,
+        packagesSold: 0,
+    }).save();
+
+    new Vacation({
+        name: 'Rock Climbing in Bend',
+        slug: 'rock-climbing-in-bend',
+        category: 'Adventure',
+        sku: 'B99',
+        description: 'Experience the thrill of rock climbing in the high desert.',
+        priceInCents: 289995,
+        tags: ['weekend getaway', 'bend', 'high desert', 'rock climbing', 'hiking', 'skiing'],
+        inSeason: true,
+        requiresWaiver: true,
+        maximumGuests: 4,
+        available: false,
+        packagesSold: 0,
+        notes: 'The tour guide is currently recovering from a skiing accident.',
+    }).save();
+});
 
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.env.PORT || 3000);
+app.use(require('body-parser').urlencoded({extended: false}));
+
+app.use(function(req, res, next){
+    // create a domain for this request
+    var domain = require('domain').create();
+    // handle errors on this domain
+    domain.on('error', function(err){
+        console.error('Перехвачена ошибка домена\n', err.stack);
+        try {
+            // failsafe shutdown in 5 seconds
+            setTimeout(function(){
+                console.error('Отказобезопаснывй останов.');
+                process.exit(1);
+            }, 5000);
+
+            // disconnect from the cluster
+            var worker = require('cluster').worker;
+            if(worker) worker.disconnect();
+
+            // stop taking new requests
+            server.close();
+
+            try {
+                // attempt to use Express error route
+                next(err);
+            } catch(error){
+                // if Express error route failed, try
+                // plain Node response
+                console.error('Сбой механизма обработки ошибок.\n', error.stack);
+                res.statusCode = 500;
+                res.setHeader('content-type', 'text/plain');
+                res.end('Ошибка сервера.');
+            }
+        } catch(error){
+            console.error('Не могу отправить ошибку 500.\n', error.stack);
+        }
+    });
+
+    // add the request and response objects to the domain
+    domain.add(req);
+    domain.add(res);
+
+    // execute the rest of the request chain in the domain
+    domain.run(next);
+});
 
 app.use(express.static(__dirname + '/public'));
-app.use(require('body-parser').urlencoded({extended: true}));
+app.use('/api', require('cors')());
+
+app.use(require('body-parser').json());
+
 app.use(require('cookie-parser')(credentials.cookiesSecret));
 app.use(require('express-session')({
     resave: false,
     saveUninitialized: false,
-    secret: 'Секретный файл'
+    secret: credentials.cookiesSecret,
+    store: sessionStore
 }));
 app.use(function(req, res, next){
     // if there's a flash message, transfer
@@ -44,11 +166,106 @@ app.use(function (req, res, next) {
     next();
 });
 
+
+switch (app.get('env')){
+    case 'development':
+        app.use(require('morgan')('dev'));
+        break;
+    case 'productoin':
+        app.use(require('express-logger')({
+            path: __dirname + '/log/requests.log'
+        }));
+        break;
+}
+
+
+app.use(function (req,res,next) {
+    var cluster = require('cluster');
+    if(cluster.isWorker)console.log('Исполнитель %d получил запрос ', cluster.worker.id);
+    next();
+});
+
+app.use(function (req,res,next) {
+    if(req.body) console.log(req.body)
+    next();
+});
+
+
+
+
+var admin = express.Router();
+app.use(require('vhost')('admin.*', admin));
+
+admin.get('/', function(req, res){
+    res.render('admin/home');
+});
+admin.get('/users', function(req, res){
+    res.render('admin/users');
+});
+
+
+var Attraction = require('./models/attraction.js');
+
+app.get('/api/attractions', function(req, res){
+    Attraction.find({ approved: true }, function(err, attractions){
+        if(err) return res.status(500).send('Произошла ошибка базы данных');
+        res.json(attractions.map(function (a) {
+            return {
+                name: a.name,
+                id: a._id,
+                description: a.description,
+                location: a.location
+            }
+        }))
+    });
+});
+
+app.post('/api/attraction', function(req, res){
+
+    var a = new Attraction({
+        name: req.body.name,
+        description: req.body.description,
+        location: { lat: req.body.lat, lng: req.body.lng },
+        history: {
+            event: 'created',
+            email: req.body.email,
+            date: new Date(),
+        },
+        approved: false,
+    });
+
+    a.save(function(err, a){
+        if(err) return res.status(500).send('Произошла ошибка базы данных');
+        res.json({id: a._id});
+    });
+});
+
+app.get('/api/attraction/:id', function(req, res){
+    Attraction.findById(req.params.id, function(err, a){
+        if(err) return res.status(500).send('Произошла ошибка базы данных');
+        res.json({
+            name: a.name,
+            id: a._id,
+            description: a.description,
+            location: a.location
+        });
+    });
+});
+
+
+
+
 app.get('/', function (req, res) {
     req.session.name = 'Anonimus';
     var colorShema = req.session.colorShema || 'dark';
     console.log(req.session.name);
     res.render('home');
+});
+
+app.get('/epic-file', function (req, res) {
+    process.nextTick(function () {
+        throw new Error('эпик фейл');
+    });
 });
 
 app.get('/about', function (req, res) {
@@ -124,7 +341,7 @@ NewsletterSignup.prototype.save = function(cb){
     cb();
 };
 
-var VALID_EMAIL_REGEX = /^\/d/;
+var VALID_EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)+$/;
 
 app.post('/newsletter', function(req, res){
     var name = req.body.name || '', email = req.body.email || '';
@@ -138,7 +355,7 @@ app.post('/newsletter', function(req, res){
         };
         return res.redirect(303, '/newsletter/archive');
     }
-    new NewsletterSignup({ name: name, email: email }).save(function(err){
+    VacationInSeasonListener({ name: name, email: email }).save(function(err){
         if(err) {
             if(req.xhr) return res.json({ error: 'Database error.' });
             req.session.flash = {
@@ -158,6 +375,80 @@ app.post('/newsletter', function(req, res){
     });
 });
 
+app.get('/newsletter/archive', function(req, res){
+    res.render('newsletter/archive');
+});
+
+app.get('/vacations', function(req, res){
+    Vacation.find({ available: true }, function(err, vacations){
+        var currency = req.session.currency || 'USD';
+        var context = {
+            currency: currency,
+            vacations: vacations.map(function(vacation){
+                return {
+                    sku: vacation.sku,
+                    name: vacation.name,
+                    description: vacation.description,
+                    inSeason: vacation.inSeason,
+                    price: convertFromUSD(vacation.priceInCents/100, currency),
+                    qty: vacation.qty,
+                };
+            })
+        };
+        switch(currency){
+            case 'USD': context.currencyUSD = 'selected'; break;
+            case 'GBP': context.currencyGBP = 'selected'; break;
+            case 'BTC': context.currencyBTC = 'selected'; break;
+        }
+        res.render('vacations', context);
+    });
+});
+
+function convertFromUSD(value, currency){
+    switch(currency){
+        case 'USD': return value * 1;
+        case 'GBP': return value * 0.6;
+        case 'BTC': return value * 0.0023707918444761;
+        default: return NaN;
+    }
+}
+
+
+app.get('/notify-me-when-in-season', function(req, res){
+    res.render('notify-me-when-in-season', { sku: req.query.sku });
+});
+
+app.post('/notify-me-when-in-season', function(req, res){
+    VacationInSeasonListener.update(
+        { email: req.body.email },
+        { $push: { skus: req.body.sku } },
+        { upsert: true },
+        function(err){
+            if(err) {
+                console.error(err.stack);
+                req.session.flash = {
+                    type: 'danger',
+                    intro: 'Ooops!',
+                    message: 'There was an error processing your request.',
+                };
+                return res.redirect(303, '/vacations');
+            }
+            req.session.flash = {
+                type: 'success',
+                intro: 'Thank you!',
+                message: 'You will be notified when this vacation is in season.',
+            };
+            return res.redirect(303, '/vacations');
+        }
+    );
+});
+
+
+app.get('/set-currency/:currency', function(req,res){
+    req.session.currency = req.params.currency;
+    return res.redirect(303, '/vacations');
+});
+
 
 app.use(function (req, res) {
     res.status(404);
@@ -170,8 +461,26 @@ app.use(function (err, req, res, next) {
     res.render('500');
 });
 
-console.log(app.get(''))
 
-app.listen(app.get('port'), function () {
-    console.log('Express запущен на порту ' + app.get('port') + " " + app.get('env'));
-});
+
+
+
+var server;
+
+function startServer() {
+    app.listen(app.get('port'), function () {
+        console.log('Express запущен на порту ' + app.get('port') + " " + app.get('env'));
+    });
+}
+
+if(require.main === module){
+    // application run directly; start app server
+    startServer();
+} else {
+    // application imported as a module via "require": export function to create server
+    module.exports = startServer;
+}
+
+
+
+
